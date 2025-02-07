913b80e33ef42881460ecc33d025512f4f1eac58
wesleysotnas64 <wesleysotnas64@gmail.com>
2025-02-07 14:45:08 -0300

    fix: Adjust four direction move and movement speed.

--- Fruit Defenders/Assets/Scripts/PlayerController.cs
+++ Fruit Defenders/Assets/Scripts/PlayerController.cs
@@ -21,11 +21,11 @@ public class PlayerController : MonoBehaviour
     private void Movement()
     {
         direction = Vector3.zero;
-        
-        // if(Input.GetKey(KeyCode.W)) direction += new Vector3( 0,  1, 0);
-        // if(Input.GetKey(KeyCode.S)) direction += new Vector3( 0, -1, 0);
+
         if(Input.GetKey(KeyCode.A)) direction += new Vector3(-1,  0, 0);
         if(Input.GetKey(KeyCode.D)) direction += new Vector3( 1,  0, 0);
+        if(Input.GetKey(KeyCode.W)) direction += new Vector3( 0,  1, 0);
+        if(Input.GetKey(KeyCode.S)) direction += new Vector3( 0, -1, 0);
 
         playerMove.SetMovement(direction, speed);
     }
