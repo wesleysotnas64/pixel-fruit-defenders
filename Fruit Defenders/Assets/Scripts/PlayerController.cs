c8883bd13400b39ab67c78c9614e5fe1a2150834
wesleysotnas64 <wesleysotnas64@gmail.com>
2025-02-07 15:00:11 -0300

    feat: Adjust screen aspect and limit screen movement.

--- Fruit Defenders/Assets/Scripts/PlayerController.cs
+++ Fruit Defenders/Assets/Scripts/PlayerController.cs
@@ -6,10 +6,17 @@ public class PlayerController : MonoBehaviour
     public float speed;
     private Move playerMove;
     private ProjectileManager projectileManager;
+
+    private Vector2 minPointLimit;
+    private Vector2 maxPointLimit;
+    private float xPosition;
+    private float yPosition;
     void Start()
     {
         playerMove = GetComponent<Move>();
         projectileManager = GetComponent<ProjectileManager>();
+        minPointLimit = new Vector2(-5.5f, -9.25f);
+        maxPointLimit = new Vector2( 5.5f,  9.25f);
     }
 
     void Update()
@@ -20,12 +27,15 @@ public class PlayerController : MonoBehaviour
 
     private void Movement()
     {
+        xPosition = transform.position.x;
+        yPosition = transform.position.y;
+
         direction = Vector3.zero;
 
-        if(Input.GetKey(KeyCode.A)) direction += new Vector3(-1,  0, 0);
-        if(Input.GetKey(KeyCode.D)) direction += new Vector3( 1,  0, 0);
-        if(Input.GetKey(KeyCode.W)) direction += new Vector3( 0,  1, 0);
-        if(Input.GetKey(KeyCode.S)) direction += new Vector3( 0, -1, 0);
+        if(Input.GetKey(KeyCode.A) && xPosition > minPointLimit.x) direction += new Vector3(-1,  0, 0);
+        if(Input.GetKey(KeyCode.D) && xPosition < maxPointLimit.x) direction += new Vector3( 1,  0, 0);
+        if(Input.GetKey(KeyCode.W) && yPosition < maxPointLimit.y) direction += new Vector3( 0,  1, 0);
+        if(Input.GetKey(KeyCode.S) && yPosition > minPointLimit.y) direction += new Vector3( 0, -1, 0);
 
         playerMove.SetMovement(direction, speed);
     }
